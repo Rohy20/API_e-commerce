c493bc7e83eca9f3c8f5b199360d2b7e167edd20
Rohy Maia <rohymaia@outlook.com>
2023-07-11 20:25:54 -0300

    Adicionando parte inicial do projeto

---
+++ src/categories/category_service.js
@@ -0,0 +1,8 @@
+exports.showcategories = function(){
+  return [
+    'Smartphones', 
+    'Acessórios', 
+    'Computadores', 
+    'Monitores', 
+    'Teclados'];
+}
---
+++ src/products/product_service.js
@@ -0,0 +1,20 @@
+exports.showProducts = function() {
+    //const products = 
+    return [{
+      name: 'iPhone 14',
+      price: 9500,
+      category: 'Smartphones'
+    },{
+      name: 'Samsung S23',
+      price: 6500,
+      category: 'Smartphones'
+    },{
+      name: 'monitor',
+      price: 3000,
+      category: 'Monitores',
+      active: false,
+    }];
+    //return products.filter((product) => {
+      //return product.price > param;
+   // });
+  }
